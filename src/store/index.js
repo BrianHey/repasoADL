3319e8b1174fb94dbe5956ce7659c911ead52094
Raul Catalan <raulcatalan95@gmail.com>
2020-08-27 12:12:44 -0400

    cuarto commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,37 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import axios from "axios";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    variable1: "Hola Soy Seba",
+    variable1: true,
+    user: {},
   },
   mutations: {
     CAMBIOVALOR(state) {
-      state.variable1 = "HOla soy seba 2";
+      state.variable1 = !state.variable1;
+    },
+    SET_USER(state, newuser) {
+      state.user = newuser;
     },
   },
   actions: {
     cambioValor({ commit }) {
       commit("CAMBIOVALOR");
+    },
+
+    llamarApi({ commit }) {
+      axios.get("https://randomuser.me/api/").then((data) => {
+        console.log(data.data);
+        let user = data.data.results[0];
+
+        let payload = {
+          fullName: `${user.name.title} ${user.name.first} ${user.name.last} `,
+          foto_url: user.picture.medium
+        };
+        commit("SET_USER", payload);
+      });
     },
   },
   modules: {},
--- src/views/Caramelitos.vue
+++ src/views/Caramelitos.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
     <h1>Soy un caramelito</h1>
-    <h3>{{variable1}}</h3>
+    <h3>{{cambiarVariable1}}</h3>
     <button @click="cambioValor">Cambio de valor</button>
   </div>
 </template>
 <script>
 import { mapState, mapActions } from "vuex";
 
+
 export default {
   computed: {
     ...mapState(["variable1"]),
+    cambiarVariable1(){
+     return this.variable1 ? "1" : "2";
+    }
   },
   methods: {
-    ...mapActions(["cambioValor"]),
+    ...mapActions(["cambioValor","llamarApi"]),
+
   },
+  created(){
+    this.llamarApi();
+  }
 };
 </script>

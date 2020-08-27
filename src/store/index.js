4abf463b93157ee7da7c87b9a403bf8156715d2f
Sebastian Urdaneta Clermont <sebastianurdanetaclermont@MacBook-Pro-de-Sebastian.local>
2020-08-27 11:37:10 -0400

    tercer commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    variable1: "Hola Soy Seba",
   },
   mutations: {
+    CAMBIOVALOR(state) {
+      state.variable1 = "HOla soy seba 2";
+    },
   },
   actions: {
+    cambioValor({ commit }) {
+      commit("CAMBIOVALOR");
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- src/views/Caramelitos.vue
+++ src/views/Caramelitos.vue
@@ -1,5 +1,19 @@
 <template>
-<div>
-  <h1>Soy un caramelito</h1>
-</div>
+  <div>
+    <h1>Soy un caramelito</h1>
+    <h3>{{variable1}}</h3>
+    <button @click="cambioValor">Cambio de valor</button>
+  </div>
 </template>
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["variable1"]),
+  },
+  methods: {
+    ...mapActions(["cambioValor"]),
+  },
+};
+</script>

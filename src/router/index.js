a63157744aa185d949ea85bd0f0380000d155827
Brian <brian.habib@brandnlabel.com>
2020-08-27 11:21:10 -0400

    second commit

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,11 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
     path: '/about',
@@ -16,14 +16,22 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/caramelitos',
+    name: 'Caramelitos',
+    // route level code-splitting
+    // this generates a separate chunk (Caramelitos.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "Caramelitos" */ '../views/Caramelitos.vue'),
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 })
 
 export default router
---
+++ src/views/Caramelitos.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+  <h1>Soy un caramelito</h1>
+</div>
+</template>

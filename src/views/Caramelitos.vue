101c1162d59d384671d2496e67700a54d8bccb45
rohmedina <ro.medina.gatica@gmail.com>
2020-08-27 11:45:32 -0400

    se agrega bootstrap

--- src/views/Caramelitos.vue
+++ src/views/Caramelitos.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <h1>Soy un caramelito</h1>
-    <h3>{{variable1}}</h3>
-    <button @click="cambioValor">Cambio de valor</button>
-  </div>
+<div>
+  <h1>Soy un caramelito</h1>
+  <h3>{{ variable1 }}</h3>
+  <button @click="cambioValor" class="btn btn-success">Cambio de valor</button>
+</div>
 </template>
+
 <script>
-import { mapState, mapActions } from "vuex";
+import {
+  mapState,
+  mapActions
+} from "vuex";
 
 export default {
   computed: {

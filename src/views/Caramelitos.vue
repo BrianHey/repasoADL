a6c7f1907a4e17f406c1012661270d114dbdc979
rohmedina <ro.medina.gatica@gmail.com>
2020-08-28 21:30:14 -0400

    Detalles curso pendiente

--- src/views/Caramelitos.vue
+++ src/views/Caramelitos.vue
@@ -1,41 +1,63 @@
 <template>
-  <div>
-    <div class="wrapper cartas px-5">
-      <div class="row">
-        <div class="col-md-3 " v-for="(signo, i) of horoscopos" :key="i">
-          <b-card no-body style="max-width: 20rem;" :img-src="imagenGenerica" img-alt="Image" img-top>
-            <template v-slot:header>
-              <h4 class="mb-0">{{ signo.nombre }}</h4>
-            </template>
+<div>
+  <div class="pb-5">
+    <input type="date" />
+    <button class="btn btn-success">Mi signo</button>
 
-            <b-card-body>
-              <b-card-title>Amor</b-card-title>
-              <b-card-text>{{ signo.amor }}</b-card-text>
-            </b-card-body>
+    <h3>Respuesta</h3>
+  </div>
 
-            <b-card-body>
-              <b-card-title>Dinero</b-card-title>
-              <b-card-text>{{ signo.dinero }}</b-card-text>
-            </b-card-body>
-
-            <b-list-group flush>
-              <b-list-group-item>{{ signo.fechaSigno }}</b-list-group-item>
-              <b-list-group-item>{{ signo.color }}</b-list-group-item>
-              <b-list-group-item>Numero: {{ signo.numero }}</b-list-group-item>
-            </b-list-group>
-          </b-card>
-        </div>
+  <div class="wrapper cursos px-5 pt-5">
+    <h4>Cursos</h4>
+    <div class="row">
+      <div class="col-md-4" v-for="(curso, i) of cursos" :key="i">
+        <b-card :title="curso.nombre" :img-src="curso.cover" img-alt="Image" img-top tag="article" style="max-width: 20rem" class="mb-2">
+          <b-card-text> {{ curso.slug }} </b-card-text>
+        </b-card>
       </div>
     </div>
   </div>
+
+  <div class="wrapper cartas px-5 pt-5">
+    <h4>Horoscopo Diario</h4>
+    <div class="row">
+      <div class="col-md-3" v-for="(signo, i) of horoscopos" :key="i">
+        <b-card no-body style="max-width: 20rem" :img-src="imagenGenerica" img-alt="Image" img-top>
+          <template v-slot:header>
+            <h4 class="mb-0">{{ signo.nombre }}</h4>
+          </template>
+
+          <b-card-body>
+            <b-card-title>Amor</b-card-title>
+            <b-card-text>{{ signo.amor }}</b-card-text>
+          </b-card-body>
+
+          <b-card-body>
+            <b-card-title>Dinero</b-card-title>
+            <b-card-text>{{ signo.dinero }}</b-card-text>
+          </b-card-body>
+
+          <b-list-group flush>
+            <b-list-group-item>{{ signo.fechaSigno }}</b-list-group-item>
+            <b-list-group-item>{{ signo.color }}</b-list-group-item>
+            <b-list-group-item>Numero: {{ signo.numero }}</b-list-group-item>
+          </b-list-group>
+        </b-card>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import {
+  mapState,
+  mapActions
+} from "vuex";
 
 export default {
   computed: {
-    ...mapState(["variable1", "user", "horoscopos"]),
+    ...mapState(["variable1", "user", "horoscopos", "cursos"]),
     cambiarVariable1() {
       return this.variable1 ? "1" : "2";
     },
@@ -46,13 +68,14 @@ export default {
     };
   },
   methods: {
-    ...mapActions(["cambioValor", "llamarApi", "llamarHoroscopos"]),
+    ...mapActions(["cambioValor", "llamarApi", "llamarHoroscopos", "llamarCursos"]),
   },
   created() {
     this.llamarApi();
   },
   mounted() {
     this.llamarHoroscopos();
+    this.llamarCursos();
   },
 };
 </script>
@@ -60,5 +83,10 @@ export default {
 <style lang="scss" scoped>
 img {
   border-radius: 1rem;
+}
+
+.cursos .card {
+  border: 1px solid red;
+  height: 300px;
 }
 </style>

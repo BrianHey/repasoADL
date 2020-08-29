6355648a8051826cee967502277dc83c6c46a6ad
Raul Catalan <raulcatalan95@gmail.com>
2020-08-28 20:55:38 -0400

    cartas cursos

--- src/store/index.js
+++ src/store/index.js
@@ -1,51 +1,62 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios'
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     variable1: true,
     user: {},
-    horoscopos: [],
+    cursos: [],
   },
   mutations: {
     CAMBIOVALOR(state) {
-      state.variable1 = !state.variable1
+      state.variable1 = !state.variable1;
     },
     SET_USER(state, newuser) {
-      state.user = newuser
+      state.user = newuser;
     },
-    SET_HOROSCOPOS(state, horoscopos) {
-      state.horoscopos = horoscopos
+    SET_CURSOS(state, cursos) {
+      state.cursos = cursos;
     },
   },
   actions: {
     cambioValor({ commit }) {
-      commit('CAMBIOVALOR')
+      commit("CAMBIOVALOR");
     },
 
     llamarApi({ commit }) {
-      axios.get('https://randomuser.me/api/').then((data) => {
-        console.log(data.data)
-        let user = data.data.results[0]
+      axios.get("https://randomuser.me/api/").then((data) => {
+        console.log(data.data);
+        let user = data.data.results[0];
 
         let payload = {
           fullName: `${user.name.title} ${user.name.first} ${user.name.last} `,
           foto_url: user.picture.large,
-        }
-        commit('SET_USER', payload)
-      })
+        };
+        commit("SET_USER", payload);
+      });
     },
 
-    llamarHoroscopos({ commit }) {
-      axios.get('https://api.xor.cl/tyaas/').then((data) => {
-        console.log(data.data)
-        let res = data.data.horoscopo
-        let miDataHoroscopo = Object.values(res)
-        commit('SET_HOROSCOPOS', miDataHoroscopo)
-      })
+    llamarCursos({ commit }) {
+      axios
+        .get(
+          "https://cors-anywhere.herokuapp.com/https://pruebafrontend.desafiolatam.com/api/v1/courses"
+        )
+        .then((data) => {
+          console.log(data.data.data);
+          let cursos = [];
+          data.data.data.forEach((e) => {
+            cursos.push({
+              nombre: e.attributes.name,
+              slug: e.attributes.slug,
+              cover: e.attributes.cover,
+            });
+          });
+        console.log(cursos);
+          commit("SET_CURSOS", cursos);
+        });
     },
   },
   modules: {},
-})
+});
--- src/views/Caramelitos.vue
+++ src/views/Caramelitos.vue
@@ -1,28 +1,21 @@
 <template>
   <div>
+    <h1>holaaaa</h1>
     <div class="wrapper cartas px-5">
       <div class="row">
-        <div class="col-md-3 " v-for="(signo, i) of horoscopos" :key="i">
-          <b-card no-body style="max-width: 20rem;" :img-src="imagenGenerica" img-alt="Image" img-top>
-            <template v-slot:header>
-              <h4 class="mb-0">{{ signo.nombre }}</h4>
-            </template>
+        <div class="col-md-3 " v-for="(curso, i) of cursos" :key="i">
+          <b-card
+            :title="curso.nombre"
+            :img-src="curso.cover"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text> </b-card-text>
 
-            <b-card-body>
-              <b-card-title>Amor</b-card-title>
-              <b-card-text>{{ signo.amor }}</b-card-text>
-            </b-card-body>
-
-            <b-card-body>
-              <b-card-title>Dinero</b-card-title>
-              <b-card-text>{{ signo.dinero }}</b-card-text>
-            </b-card-body>
-
-            <b-list-group flush>
-              <b-list-group-item>{{ signo.fechaSigno }}</b-list-group-item>
-              <b-list-group-item>{{ signo.color }}</b-list-group-item>
-              <b-list-group-item>Numero: {{ signo.numero }}</b-list-group-item>
-            </b-list-group>
+            <b-button href="#" variant="primary">Go somewhere</b-button>
           </b-card>
         </div>
       </div>
@@ -35,24 +28,25 @@ import { mapState, mapActions } from "vuex";
 
 export default {
   computed: {
-    ...mapState(["variable1", "user", "horoscopos"]),
+    ...mapState(["variable1", "user", "cursos"]),
     cambiarVariable1() {
       return this.variable1 ? "1" : "2";
     },
   },
   data() {
     return {
-      imagenGenerica: "https://estaticos.elperiodico.com/resources/jpg/8/2/signos-del-zodiaco-del-horoscopo-diario-1568735910628.jpg",
+      imagenGenerica:
+        "https://estaticos.elperiodico.com/resources/jpg/8/2/signos-del-zodiaco-del-horoscopo-diario-1568735910628.jpg",
     };
   },
   methods: {
-    ...mapActions(["cambioValor", "llamarApi", "llamarHoroscopos"]),
+    ...mapActions(["cambioValor", "llamarApi", "llamarCursos"]),
   },
   created() {
     this.llamarApi();
   },
   mounted() {
-    this.llamarHoroscopos();
+    this.llamarCursos();
   },
 };
 </script>

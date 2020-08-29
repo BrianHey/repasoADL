3d84290680ca82b39a0f489202524ef8ac541b10
Brian <brian.habib@brandnlabel.com>
2020-08-28 21:49:29 -0400

    regalito con logica sobre lo ilogico

--- src/store/index.js
+++ src/store/index.js
@@ -1,83 +1,79 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import axios from "axios";
-Vue.use(Vuex);
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     variable1: true,
     user: {},
-<<<<<<< HEAD
-    horoscopos: [],
-=======
->>>>>>> 6355648a8051826cee967502277dc83c6c46a6ad
     cursos: [],
   },
   mutations: {
     CAMBIOVALOR(state) {
-      state.variable1 = !state.variable1;
+      state.variable1 = !state.variable1
     },
     SET_USER(state, newuser) {
-      state.user = newuser;
+      state.user = newuser
     },
-<<<<<<< HEAD
-    SET_HOROSCOPOS(state, horoscopos) {
-      state.horoscopos = horoscopos;
-    },
-=======
->>>>>>> 6355648a8051826cee967502277dc83c6c46a6ad
     SET_CURSOS(state, cursos) {
-      state.cursos = cursos;
+      state.cursos = cursos
     },
   },
   actions: {
     cambioValor({ commit }) {
-      commit("CAMBIOVALOR");
+      commit('CAMBIOVALOR')
     },
 
-    llamarApi({ commit }) {
-      axios.get("https://randomuser.me/api/").then((data) => {
-        console.log(data.data);
-        let user = data.data.results[0];
+    // llamarApi({ commit }) {
+    //   axios.get('https://randomuser.me/api/').then((data) => {
+    //     console.log(data.data)
+    //     let user = data.data.results[0]
 
-        let payload = {
-          fullName: `${user.name.title} ${user.name.first} ${user.name.last} `,
-          foto_url: user.picture.large,
-        };
-        commit("SET_USER", payload);
-      });
-    },
+    //     let payload = {
+    //       fullName: `${user.name.title} ${user.name.first} ${user.name.last} `,
+    //       foto_url: user.picture.large,
+    //     }
+    //     commit('SET_USER', payload)
+    //   })
+    // },
 
     llamarHoroscopos({ commit }) {
-      axios.get("https://api.xor.cl/tyaas/").then((data) => {
-        console.log(data.data);
-        let res = data.data.horoscopo;
-        let miDataHoroscopo = Object.values(res);
-        commit("SET_HOROSCOPOS", miDataHoroscopo);
-      });
+      axios.get('https://api.xor.cl/tyaas/').then((data) => {
+        console.log(data.data)
+        let res = data.data.horoscopo
+        let miDataHoroscopo = Object.values(res)
+        commit('SET_HOROSCOPOS', miDataHoroscopo)
+      })
     },
 
     llamarCursos({ commit }) {
-      let cursos = [];
+      let cursos = []
       axios
-        .get("https://cors-anywhere.herokuapp.com/https://pruebafrontend.desafiolatam.com/api/v1/courses")
+        .get('https://cors-anywhere.herokuapp.com/https://pruebafrontend.desafiolatam.com/api/v1/courses')
         .then((data) => {
-          //console.log(data.data.data);
-
           data.data.data.forEach((e) => {
-            cursos.push({ nombre: e.attributes.name, slug: e.attributes.slug, cover: e.attributes.cover });
-            commit("SET_CURSOS", cursos);
-          });
+            cursos.push({ nombre: e.attributes.name, slug: e.attributes.slug, cover: e.attributes.cover })
+          })
         })
         .then((data) => {
-          axios.get("https://cors-anywhere.herokuapp.com/https://pruebafrontend.desafiolatam.com/api/v1/users/me").then((data) => {
-            console.log(data.data.included);
-            let detalleCursos = data.data.included;
-            detalleCursos.forEach((e) => {});
-          });
-        });
+          axios
+            .get('https://cors-anywhere.herokuapp.com/https://pruebafrontend.desafiolatam.com/api/v1/users/me')
+            .then((data) => {
+              let detalleCursos = data.data.included
+              console.log(detalleCursos)
+              console.log(cursos)
+
+              cursos.forEach((c, i) => {
+                detalleCursos.forEach((d) => {
+                  d.attributes.slug == c.slug ? (cursos[i].unidades = d.attributes.learning) : false
+                })
+              })
+              commit('SET_CURSOS', cursos)
+            })
+        })
       // console.log(cursos);
     },
   },
   modules: {},
-});
+})
--- src/views/Caramelitos.vue
+++ src/views/Caramelitos.vue
@@ -1,73 +1,23 @@
 <template>
-<<<<<<< HEAD
 <div>
-  <div class="pb-5">
-    <input type="date" />
-    <button class="btn btn-success">Mi signo</button>
-
-    <h3>Respuesta</h3>
-  </div>
-
-  <div class="wrapper cursos px-5 pt-5">
-    <h4>Cursos</h4>
+  <div class="wrapper cartas px-5">
     <div class="row">
-      <div class="col-md-4" v-for="(curso, i) of cursos" :key="i">
-        <b-card :title="curso.nombre" :img-src="curso.cover" img-alt="Image" img-top tag="article" style="max-width: 20rem" class="mb-2">
-          <b-card-text> {{ curso.slug }} </b-card-text>
+      <div class="col-md-3 " v-for="(curso, i) of cursos" :key="i">
+        <b-card :title="curso.nombre" :img-src="curso.cover" img-alt="Image" img-top tag="article" style="max-width: 20rem;" class="mb-2">
+          <b-card-text>
+
+            <ul>
+
+              <li v-for="(unidad, index) in curso.unidades" :key="index"> {{unidad.name}}</li>
+            </ul>
+          </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
         </b-card>
       </div>
     </div>
   </div>
-
-  <div class="wrapper cartas px-5 pt-5">
-    <h4>Horoscopo Diario</h4>
-    <div class="row">
-      <div class="col-md-3" v-for="(signo, i) of horoscopos" :key="i">
-        <b-card no-body style="max-width: 20rem" :img-src="imagenGenerica" img-alt="Image" img-top>
-          <template v-slot:header>
-            <h4 class="mb-0">{{ signo.nombre }}</h4>
-          </template>
-
-          <b-card-body>
-            <b-card-title>Amor</b-card-title>
-            <b-card-text>{{ signo.amor }}</b-card-text>
-          </b-card-body>
-
-          <b-card-body>
-            <b-card-title>Dinero</b-card-title>
-            <b-card-text>{{ signo.dinero }}</b-card-text>
-          </b-card-body>
-
-          <b-list-group flush>
-            <b-list-group-item>{{ signo.fechaSigno }}</b-list-group-item>
-            <b-list-group-item>{{ signo.color }}</b-list-group-item>
-            <b-list-group-item>Numero: {{ signo.numero }}</b-list-group-item>
-          </b-list-group>
-        </b-card>
-=======
-  <div>
-    <h1>holaaaa</h1>
-    <div class="wrapper cartas px-5">
-      <div class="row">
-        <div class="col-md-3 " v-for="(curso, i) of cursos" :key="i">
-          <b-card
-            :title="curso.nombre"
-            :img-src="curso.cover"
-            img-alt="Image"
-            img-top
-            tag="article"
-            style="max-width: 20rem;"
-            class="mb-2"
-          >
-            <b-card-text> </b-card-text>
-
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-          </b-card>
-        </div>
->>>>>>> 6355648a8051826cee967502277dc83c6c46a6ad
-      </div>
-    </div>
-  </div>
+</div>
 </div>
 </template>
 
@@ -79,36 +29,23 @@ import {
 
 export default {
   computed: {
-<<<<<<< HEAD
-    ...mapState(["variable1", "user", "horoscopos", "cursos"]),
-=======
     ...mapState(["variable1", "user", "cursos"]),
->>>>>>> 6355648a8051826cee967502277dc83c6c46a6ad
     cambiarVariable1() {
       return this.variable1 ? "1" : "2";
     },
   },
   data() {
     return {
-      imagenGenerica:
-        "https://estaticos.elperiodico.com/resources/jpg/8/2/signos-del-zodiaco-del-horoscopo-diario-1568735910628.jpg",
+      imagenGenerica: "https://estaticos.elperiodico.com/resources/jpg/8/2/signos-del-zodiaco-del-horoscopo-diario-1568735910628.jpg",
     };
   },
   methods: {
-<<<<<<< HEAD
-    ...mapActions(["cambioValor", "llamarApi", "llamarHoroscopos", "llamarCursos"]),
-=======
     ...mapActions(["cambioValor", "llamarApi", "llamarCursos"]),
->>>>>>> 6355648a8051826cee967502277dc83c6c46a6ad
   },
   created() {
     this.llamarApi();
   },
   mounted() {
-<<<<<<< HEAD
-    this.llamarHoroscopos();
-=======
->>>>>>> 6355648a8051826cee967502277dc83c6c46a6ad
     this.llamarCursos();
   },
 };
